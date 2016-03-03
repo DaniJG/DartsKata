ee7dab9228cc4ca842abfa5da7cdf6b2b18a53e4
Daniel Jimenez <daniel.jimenez.garcia@ericsson.com>
2016-03-03 16:01:51 +0000

    refactor tests

--- DartsKata.Test/DartsGameTest.cs
+++ DartsKata.Test/DartsGameTest.cs
@@ -7,55 +7,45 @@ namespace DartsKata.Test
     [TestClass]
     public class DartsGameTest
     {
+        private Mock<IPlayer> _player1;
+        private Mock<IPlayer> _player2;
+        private DartsGame _dartsGame;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _player1 = new Mock<IPlayer>();
+            _player1.SetupGet(p => p.Score).Returns(2);
+            _player2 = new Mock<IPlayer>();
+            _player2.SetupGet(p => p.Score).Returns(16);
+
+            _dartsGame = new DartsGame(new IPlayer[] { _player1.Object, _player2.Object });
+        }
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void DartsGameTest_ANewGameNeedsPlayers()
         {
-            //Act
             var dartsGame = new DartsGame(null);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void DartsGameTest_ANewGameNeedsAtLeast2Players()
-        {
-            //Arrange
-            var player1 = new Mock<IPlayer>();
-
-            //Act
-            var dartsGame = new DartsGame(new IPlayer[] { player1.Object });
+        {            
+            var dartsGame = new DartsGame(new IPlayer[] { _player1.Object });
         }
 
         [TestMethod]
         public void DartsGameTest_ANewGameIsNotFinished()
-        {
-            //Arrange
-            var player1 = new Mock<IPlayer>();
-            player1.SetupGet(p => p.Score).Returns(2);
-            var player2 = new Mock<IPlayer>();
-            player2.SetupGet(p => p.Score).Returns(16);
-
-            //Arrange
-            var dartsGame = new DartsGame(new IPlayer[] { player1.Object, player2.Object });
-
-            //Act
-            Assert.IsFalse(dartsGame.Finished);
+        {            
+            Assert.IsFalse(_dartsGame.Finished);
         }
 
         [TestMethod]
         public void DartsGameTest_AGameIsNotFinished_WhileNoPlayerHasClearedHisScore()
         {
-            //Arrange
-            var player1 = new Mock<IPlayer>();
-            player1.SetupGet(p => p.Score).Returns(2);
-            var player2 = new Mock<IPlayer>();
-            player2.SetupGet(p => p.Score).Returns(16);
-
-            //Act
-            var dartsGame = new DartsGame(new IPlayer[] { player1.Object, player2.Object });
-
-            //Assert
-            Assert.IsFalse(dartsGame.Finished);
+            Assert.IsFalse(_dartsGame.Finished);
         }
     }
 }

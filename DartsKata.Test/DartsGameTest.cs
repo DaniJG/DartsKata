2c688089f00e022e1cb95049b9c8173c5c7b4342
Daniel Jimenez <daniel.jimenez.garcia@ericsson.com>
2016-03-03 15:34:38 +0000

    First test

---
+++ DartsKata.Test/DartsGameTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DartsKata.Test
+{
+    [TestClass]
+    public class DartsGameTest
+    {
+        [TestMethod]
+        public void DartsGameTest_ANewGameIsNotFinished()
+        {
+            var dartsGame = new DartsGame();
+
+            Assert.IsFalse(dartsGame.Finished);
+        }
+    }
+}

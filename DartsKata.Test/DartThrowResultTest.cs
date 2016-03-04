455a62b66442228b951f7fe1f3b6c10e050c8533
Daniel Jimenez <daniel.jimenez.garcia@ericsson.com>
2016-03-04 17:40:44 +0000

    Implemented DartThrowResult

---
+++ DartsKata.Test/DartThrowResultTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DartsKata.Test
+{
+    [TestClass]
+    public class DartThrowResultTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DartThrowResult_CannotHaveADouble_WhenScoringZeroPoints()
+        {
+            var result = new DartThrowResult(0, PointsModifier.Double);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DartThrowResult_CannotHaveATriple_WhenScoringZeroPoints()
+        {
+            var result = new DartThrowResult(0, PointsModifier.Triple);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DartThrowResult_CannotHaveADouble_WhenScoringCenterPoints()
+        {
+            var result = new DartThrowResult(25, PointsModifier.Double);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DartThrowResult_CannotHaveATriple_WhenScoringCenterPoints()
+        {
+            var result = new DartThrowResult(25, PointsModifier.Triple);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DartThrowResult_CannotHaveADouble_WhenScoringBullseyePoints()
+        {
+            var result = new DartThrowResult(50, PointsModifier.Double);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DartThrowResult_CannotHaveATriple_WhenScoringBullseyePoints()
+        {
+            var result = new DartThrowResult(50, PointsModifier.Triple);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DartThrowResult_CannotHaveScoreSmallerThanZero()
+        {
+            var result = new DartThrowResult(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DartThrowResult_CannotHaveScoreBiggerThan20_ExceptCenterAndBullseye()
+        {
+            var centerResult = new DartThrowResult(25);
+            var buulseyeResult = new DartThrowResult(50);
+            Assert.IsNotNull(centerResult);
+            Assert.IsNotNull(buulseyeResult);
+
+            var result = new DartThrowResult(21);
+        }
+
+        [TestMethod]
+        public void DartThrowResult_TotalPoints_ReturnsThePointsWithoutModifier()
+        {
+            var result = new DartThrowResult(17);
+
+            Assert.AreEqual(17, result.TotalPoints);
+        }
+
+        [TestMethod]
+        public void DartThrowResult_TotalPoints_ReturnsDoubleThePointsWithTheDoubleModifier()
+        {
+            var result = new DartThrowResult(15, PointsModifier.Double);
+
+            Assert.AreEqual(30, result.TotalPoints);
+        }
+
+        [TestMethod]
+        public void DartThrowResult_TotalPoints_ReturnsTripleThePointsWithTheTripleModifier()
+        {
+            var result = new DartThrowResult(20, PointsModifier.Triple);
+
+            Assert.AreEqual(60, result.TotalPoints);
+        }
+    }
+}
---
+++ DartsKata/DartThrowResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartsKata
+{
+    public enum PointsModifier
+    {
+        Double,
+        Triple
+    };
+
+    public class DartThrowResult: IDartThrowResult
+    {
+        private int _points;
+        private bool _isDouble;
+        private PointsModifier? _pointsModifier;
+
+        public DartThrowResult(int points, PointsModifier? pointsModifier = null)
+        {
+            if (points == 0 && pointsModifier.HasValue) throw new ArgumentOutOfRangeException("pointsModifier", "No modified is allowed when scoring 0 points");
+            if (points == 25 && pointsModifier.HasValue) throw new ArgumentOutOfRangeException("pointsModifier", "No modified is allowed when scoring 0 points");
+            if (points == 50 && pointsModifier.HasValue) throw new ArgumentOutOfRangeException("pointsModifier", "No modified is allowed when scoring 0 points");
+            if (points < 0) throw new ArgumentOutOfRangeException("points");
+            if (points > 20 && points != 25 && points != 50) throw new ArgumentOutOfRangeException("points");
+
+            this._points = points;
+            this._pointsModifier = pointsModifier;
+        }
+
+        public int TotalPoints
+        {
+            get 
+            {
+                if (this._pointsModifier == PointsModifier.Double) return this._points * 2;
+                if (this._pointsModifier == PointsModifier.Triple) return this._points * 3;
+                return this._points;
+            }
+        }
+    }
+}
